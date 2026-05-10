d18f83c978e64ad5b19fb4cf50c0c868d64ecd3b
antoniosanp <antonio.pulgarin97@gmail.com>
2026-05-10 16:17:26 -0500

    first commit

---
+++ michat/src/main/java/com/example/michat/MichatApplication.java
@@ -0,0 +1,13 @@
+package com.example.michat;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MichatApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MichatApplication.class, args);
+	}
+
+}
